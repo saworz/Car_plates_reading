53746c2b07e9b82e57f14f5c7cb077e29b7eba37
saworz <michal.czechowicz7@gmail.com>
2023-01-22 23:57:07 +0100

    setup.py update

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import find_packages, setup
+from pathlib import Path
+
+BASE_DIR = Path(__file__).parent
+with open(Path(BASE_DIR, "requirements.txt")) as file:
+    required_packages = [ln.strip() for ln in file.readlines()]
 
 setup(
-    name='src',
+    name='Car_plates_reading',
     packages=find_packages(),
     version='0.1.0',
-    description='A short description of the project.',
-    author='Your name (or your organization/company/team)',
-    license='',
+    description='Car plates reading using YOLOv5 and easyOCR',
+    author='Michal',
+    install_requires=[required_packages]
 )
